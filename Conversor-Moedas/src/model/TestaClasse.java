510d9acce10f4af97da7eb54a66efa31324bc85a
Vinícius <vinicius.campos235silva@gmail.com>
2023-08-18 21:46:16 -0300

    Commit

--- Conversor-Moedas/src/model/TestaClasse.java
+++ Conversor-Moedas/src/model/TestaClasse.java
@@ -4,7 +4,7 @@ public class TestaClasse {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Conversor de moedas");
+		System.out.println("Conversor de moedas alura");
 		
 	}
 }
